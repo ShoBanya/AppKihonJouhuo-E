8376ee09b72386855591ff4362c5967c3dbb3271
Sho Banya <sho.banya.sys@gmail.com>
2017-07-04 12:18:18 +0900

    修正内容#1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
+	static int score = 0;
 
 	private static Scanner scan;
 
@@ -21,10 +22,13 @@ public class Main {
 		ansers[2] = toi3();// 問3の回答結果
 		ansers[3] = toi4();// 問4の回答結果
 		ansers[4] = toi5();// 問5の回答結果
-		ansers[5] = toi6();// 問6の回答結果
+		//ansers[5] = toi6();// 問6の回答結果
 
 		// それぞれの回答結果のまとめ処理
-
+		for(int i = 0;i < ansers.length;i++){
+			score += ansers[i];
+		}
+		System.out.println("正解数は" + score + "です！");
 	}
 
 	/**
@@ -36,24 +40,31 @@ public class Main {
 		// 回答番号
 		int answer = 0;
 		// 解答欄
-		String[] kaitouList = new String[] { "犬派", "猫派", "サーバルキャット派" };
+		String[] kaitouList = new String[] { "1:再帰的(リカーシブ)", "2:再使用可能(リユーザブル)", "3:再入可能(リエントラント)","4:再配置可能(リロケータブル)" };
 
 		// 質問内容を表示
 		System.out.println("問1");
-		System.out.println("あなたは犬派ですか？それとも猫派ですか？");
+		System.out.println("処理が終了していないプログラムが，別のプログラムから再度呼び出されることがある。");
+        System.out.println("このプログラムが正しく実行されるために備えるべき性質はどれか。");
 
 		// 解答欄を表示
 		for (int i = 0; i < kaitouList.length; i++) {
-			System.out.println("" + i + ":" + kaitouList[i]);
+			System.out.println(kaitouList[i]);
 		}
 
 		// ユーザーが答えを入力
 		System.out.println("回答：");
-		answer = Integer.valueOf(scan.next());
-
+		int num = scan.nextInt();
+		if(num == 3){
+			System.out.println("正解です!");
+			answer = 1;
+		}
+		else{
+			System.out.println("不正解です！　答えは3:再入可能(リエントラント)");
+			 //answer = 0;
+		}
 		// 回答結果を出力
-		System.out.println("「" + kaitouList[answer] + "」が選択されました。");
-		return answer;
+                return answer;
 	}
 
 	/**
